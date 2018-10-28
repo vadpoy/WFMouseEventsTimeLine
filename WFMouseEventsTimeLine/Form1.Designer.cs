2d004d005091151b385c325546cf856cd5e4b4a8
vadpoy <vadpoy@gmail.com>
2018-10-28 19:23:00 +0300

    List of points and XML serialization

--- WFMouseEventsTimeLine/Form1.Designer.cs
+++ WFMouseEventsTimeLine/Form1.Designer.cs
@@ -4,7 +4,12 @@
     {
         /// <summary>
         /// Required designer variable.
+        /// 
         /// </summary>
+        /// 
+       
+
+
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
@@ -28,13 +33,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WFMouseEventsTimeLine/Form1.cs
+++ WFMouseEventsTimeLine/Form1.cs
@@ -1,20 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
 
 namespace WFMouseEventsTimeLine
 {
+
+
     public partial class Form1 : Form
     {
+        bool bVisualize = false;
+        List<Point> points;
+        int iCounter = 0; 
+
+
         public Form1()
         {
             InitializeComponent();
+            points = new List<Point>();
+           
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar.ToString() == "q")
+            {
+                if (bVisualize)
+                {
+                    bVisualize = false;
+                    iCounter = 0;
+
+                }
+                else
+                {
+                    bVisualize = true;
+                    dataGridView1.DataSource = null;
+                    points.Clear();
+                }
+            }
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(bVisualize)
+            {
+                Point point=new Point();
+                  
+                point .Id = iCounter; iCounter++;
+                point.dtTime = DateTime.Now.Millisecond;
+                point.X = (int) e.X;
+                point.Y = (int) e.Y;
+
+                points.Add(point);
+
+                textBox1.Text = point.Id.ToString()+" "+point.dtTime.ToString()+" "+ point.X.ToString()+" "+point.Y.ToString();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = points;
+            }
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"c:\Temp\MyTest.txt";
+
+            var serializer = new XmlSerializer(typeof(List<Point>));
+            using (var writer = new StreamWriter(path))
+            {
+                serializer.Serialize(writer, points);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              dataGridView1.DataSource = points;   
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
         }
     }
+
+    public struct Point
+    {
+        public int Id {get; set;}
+        public int dtTime { get; set;}
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
 }
+                        
